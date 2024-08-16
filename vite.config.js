ff01e009d163245c50562f493788c1f01c8f5e8a
Rashid <rashidstanikzai.net@gmail.com>
2024-08-16 20:29:54 +0500

    feat: responsiveness added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
-// import wpPlugin from "webpage-builder";
+import wpPlugin from "webpage-builder";
 
 
 export default defineConfig({
-    // plugins: [wpPlugin()],
+    plugins: [wpPlugin()],
 })
